b9872870899f0b1a3d92de3c5133825cdade7de9
Mark <wsz999843@163.com>
2024-08-14 23:21:52 +0800

    chore: add ClickToComponent

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import { ClickToComponent } from 'click-to-react-component'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <ClickToComponent />
     <App />
   </React.StrictMode>
 )
